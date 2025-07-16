25531ea46a02c86ddbd611330a118d3439037388
Dynamicpayal <pn057483@gmail.com>
2025-07-16 16:31:32 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Quiz from "./components/quiz/Quiz";
+import Confetti from "react-confetti";
+
+function App() {
+  return (
+    <>
+      <Quiz />
+    </>
+  );
+}
+
+export default App;
